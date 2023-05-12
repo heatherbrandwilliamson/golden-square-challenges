7b7d16e4071096fc030c5a91a811cf028d888392
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-12 16:03:19 +0100

    updated file

---
+++ lib/counting_words.rb
@@ -0,0 +1,8 @@
+def Count_words(string)
+    splits = string.split(" ")
+    words = splits.length.to_s
+    return words
+end
+
+# A method called count_words 
+# that takes a string as an argument and returns the number of words in that string.
