a755c633787bb20576a9a336665a8669b6ab24e2
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-16 11:49:16 +0100

    updates to ruby file

--- lib/improve_grammar.rb
+++ lib/improve_grammar.rb
@@ -1,8 +1,18 @@
-def check_grammar(text)
+def check_grammar(string)
     # Capital Letter at start
-    letter = text[0]
-    # punctuation = text[-1]
+    letter = string[0]
+    punctuation = string[-1]
     if letter.upcase == letter
-        "Capital letter is correct"
+        check_capital = "Capital letter is correct"
+    else 
+        check_capital = "Capital letter is not correct"
     end
+    if punctuation == "." || punctuation == "!" || punctuation == "?"
+        check_punctuation = " & end of sentence is correct"
+    else 
+        check_punctuation = " & end of sentence is not correct"
+    end
+    return check_capital + check_punctuation
 end 
+
+puts check_grammar("Hello world!")
