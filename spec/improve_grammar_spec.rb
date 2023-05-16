f1630907ff22c8b4a5d7da641ca24ba1138d2778
Heather <Heather@Heathers-MacBook-Pro.local>
2023-05-16 11:48:11 +0100

    further test to spec

--- spec/improve_grammar_spec.rb
+++ spec/improve_grammar_spec.rb
@@ -1,9 +1,24 @@
-require 'improve_grammar'
+require 'improve_grammar' # file name
 
-RSpec.describe "test that grammar is correct in text" do
-  it "Sentence starts with capital and ends with suitable punctuation" do
-    string = "Hello world!" 
+RSpec.describe "grammar is correct in text" do
+   it "has capital letter and correct punctuation" do
+   string = "Hello world." 
+   result = check_grammar(string)
+      expect(result).to eq "Capital letter is correct & end of sentence is correct"
+  end
+  it "has capital letter and no punctuation" do
+    string = "Hello world" 
     result = check_grammar(string)
-      expect(result).eq to "Capital letter is correct"
+       expect(result).to eq "Capital letter is correct & end of sentence is not correct"
+   end
+  it "has capital letter & no punctuation" do
+    string = "Hello world" 
+    result = check_grammar(string)
+       expect(result).to eq "Capital letter is correct & end of sentence is not correct"
+   end
+  it "has no capital letter & no punctuation" do
+    string = "hello world" 
+    result = check_grammar(string)
+       expect(result).to eq "Capital letter is not correct & end of sentence is not correct"
   end
 end
